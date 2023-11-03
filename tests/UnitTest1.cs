688baabc3851019ed63879dbc68dd43b541cbfdb
Simonas Letukas <simonas.letukas@gmail.com>
2023-11-03 11:53:44 +0200

    Unit testing works

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,21 +1,30 @@
 using System;
 using Godot;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace TestProject1
 {
     public class MyTest
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public MyTest(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         public void Test1()
         {
             int i = 0;
-            Console.WriteLine("Testsss");
+            _testOutputHelper.WriteLine("Testsss");
             GD.Print("XUnitTest");
             var main = (LocalStartup)ResourceLoader.Load<PackedScene>(LocalStartup.ScenePath).Instance();
             GD.Print(System.Threading.Thread.CurrentThread.IsBackground);
             //System.Environment.CurrentManagedThreadId
             Assert.Equal("LocalStartup", main.Name);
+            //Assert.Equal("LocalStartup1", main.Name);
         }
     }
 }
