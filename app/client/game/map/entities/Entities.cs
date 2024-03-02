79469d8358b214bdf0834af0eb050e9a4edfe9a4
Simonas Letukas <simonas.letukas@gmail.com>
2024-03-02 16:07:34 +0200

    Revert "Test suite for big units created (to be reverted later)"
    This reverts commit 0392c9fdce92f489a18bf4cf53adf9741f1d4325.

--- app/client/game/map/entities/Entities.cs
+++ app/client/game/map/entities/Entities.cs
@@ -42,12 +42,9 @@ public class Entities : YSort
         
         // Force-add units for testing
         var slaveBlueprint = Data.Instance.Blueprint.Entities.Units.Single(x => x.Id.Equals(UnitId.Slave));
-        PlaceEntity(slaveBlueprint, startingPositions[0] - new Vector2(2, 2));
+        PlaceEntity(slaveBlueprint, startingPositions[0]);
         
-        var quickdraw = Data.Instance.Blueprint.Entities.Units.Single(x => x.Id.Equals(UnitId.Quickdraw));
-        PlaceEntity(quickdraw, startingPositions[3]);
-        
-        /*var horriorBlueprint = Data.Instance.Blueprint.Entities.Units.Single(x => x.Id.Equals(UnitId.Horrior));
+        var horriorBlueprint = Data.Instance.Blueprint.Entities.Units.Single(x => x.Id.Equals(UnitId.Horrior));
         PlaceEntity(horriorBlueprint, startingPositions[1]);
         
         var marksmanBlueprint = Data.Instance.Blueprint.Entities.Units.Single(x => x.Id.Equals(UnitId.Marksman));
@@ -57,7 +54,7 @@ public class Entities : YSort
         PlaceEntity(bigBadBullBlueprint, startingPositions[0] - new Vector2(2, 2));
         
         var citadelBlueprint = Data.Instance.Blueprint.Entities.Structures.Single(x => x.Id.Equals(StructureId.Citadel));
-        PlaceEntity(citadelBlueprint, startingPositions[3]);*/
+        PlaceEntity(citadelBlueprint, startingPositions[3]);
     }
     
     public override void _ExitTree()
